58197e324df21450c2988d8e504750d72ddf7ab9
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-05 16:15:01 -0500

    Labels for all outputs

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
+console.log(`Is Sum 50? ${isSum50}`);
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log(`At least two odd numbers: ${isTwoOdd}`);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+console.log(`No numbers larger than 25: ${!isOver25}`);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+console.log(`All unique numbers: ${isUnique}`);
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
@@ -33,7 +37,8 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+console.log(`Single variable "isValid": ${isValid}`);
+// console.log(isValid);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -49,7 +54,6 @@ const divisibleByFive = (n1 % 5 == 0) && (n2 % 5 == 0) && (n3 % 5 == 0) && (n4 %
 console.log(`Divisible by 5: ${divisibleByFive}`);
 
 // Check if the first number is larger than the last. Cache the result in a variable.
-
 
 /*
   Accomplish the following arithmetic chain:
