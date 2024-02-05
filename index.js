b59f3d6d0059c41d5fbf226380ad1719214b878b
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-05 16:03:19 -0500

    Outline for Part 1 of assignment

--- index.js
+++ index.js
@@ -43,3 +43,21 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+
+
+// Check if the first number is larger than the last. Cache the result in a variable.
+
+
+/*
+  Accomplish the following arithmetic chain:
+  Subtract the first number from the second number.
+  Multiply the result by the third number.
+  Find the remainder of dividing the result by the fourth number.
+*/
+
+
+/*
+  Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+*/
